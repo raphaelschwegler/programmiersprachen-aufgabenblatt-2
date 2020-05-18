66a620bef4533ad13a4ff167d32399d07589c2b4
raphaelschwegler <raphael.schwegler@uni-weimar.de>
2020-05-18 23:07:45 +0200

    First working test 2.2

--- source/tests.cpp
+++ source/tests.cpp
@@ -1,5 +1,17 @@
 #define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
+#include "vec2.hpp"
+
+TEST_CASE("describeVec2Test", "[vec2]")
+{
+	Vec2 a;
+	Vec2 b{ 5.1f, -9.3f };
+
+	REQUIRE(0.0f == a.x);
+	REQUIRE(0.0f == a.y);
+	REQUIRE(5.1f == Approx(b.x));
+	REQUIRE(-9.3f == Approx(b.y));
+}
 
 int main(int argc, char *argv[])
 {
---
+++ source/vec2.hpp
@@ -0,0 +1,9 @@
+#ifndef VEC2_HPP
+#define VEC2_HPP
+// Vec2 data type definition
+struct Vec2
+{
+	float x = 0.0f;
+	float y = 0.0f;
+};
+#endif // VEC2_HPP
