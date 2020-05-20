7d974e5f09b521225e9fb008afea05bc366b80c4
raphaelschwegler <raphael.schwegler@uni-weimar.de>
2020-05-20 12:33:37 +0200

    Add tests for Vec 2 +

--- source/tests.cpp
+++ source/tests.cpp
@@ -13,6 +13,31 @@ TEST_CASE("describeVec2Test", "[vec2]")
 	REQUIRE(-9.3f == Approx(b.y));
 }
 
+TEST_CASE("describe_additionVec2", "[vec2]")
+{
+	Vec2 a;
+	Vec2 b{ 5.1f, -9.3f };
+	Vec2 c;
+	Vec2 d{ 3.2f, 2.1f };
+	a += c;
+	REQUIRE(a.x == Approx(0.0f));
+	REQUIRE(a.y == Approx(0.0f));
+
+	a += b;
+	REQUIRE(a.x == Approx(5.1f));
+	REQUIRE(a.y == Approx(-9.3f));
+
+	b += d;
+	REQUIRE(a.x == Approx(8.3f));
+	REQUIRE(a.y == Approx(-7.2f));
+
+	d += c;
+	REQUIRE(a.x == Approx(3.2f));
+	REQUIRE(a.y == Approx(2.1f));
+
+
+}
+
 
 
 int main(int argc, char *argv[])
--- source/vec2.cpp
+++ source/vec2.cpp
@@ -1,7 +1,8 @@
 #include "vec2.hpp"
 
 Vec2& Vec2::operator+=(Vec2 const& v) {
-
+	
+	return *this;
 }
 
 
