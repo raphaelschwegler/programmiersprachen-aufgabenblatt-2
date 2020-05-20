6ec7b79e1375125bb7bfc565650f20a84e8d4f38
raphaelschwegler <raphael.schwegler@uni-weimar.de>
2020-05-20 16:09:26 +0200

    Added tests for 2.4

--- source/tests.cpp
+++ source/tests.cpp
@@ -112,7 +112,88 @@ TEST_CASE("describe_scalarDivisVec2", "[vec2]")
 
 }
 
+TEST_CASE("describe_direktAdditionVec2", "[vec2]")
+{
+	Vec2 a;
+	Vec2 b{ 5.1f, -9.3f };
+	Vec2 c{ 6.61f, 0.1f };
+	Vec2 d;
 
+	REQUIRE((a + d).x == Approx(0.0f));
+	REQUIRE((a + d).y == Approx(0.0f));
+
+	REQUIRE((a + b).x == Approx(5.1f));
+	REQUIRE((a + b).y == Approx(-9.3f));
+
+	REQUIRE((b + c).x == Approx(11.71f));
+	REQUIRE((b + c).y == Approx(-9.2f));
+}
+
+TEST_CASE("describe_direktSubtractionVec2", "[vec2]")
+{
+	Vec2 a;
+	Vec2 b{ 5.1f, -9.3f };
+	Vec2 c{ 6.61f, 0.1f };
+	Vec2 d;
+
+	REQUIRE((a - d).x == Approx(0.0f));
+	REQUIRE((a - d).y == Approx(0.0f));
+
+	REQUIRE((a - b).x == Approx(-5.1f));
+	REQUIRE((a - b).y == Approx(9.3f));
+
+	REQUIRE((b - c).x == Approx(-1.51f));
+	REQUIRE((b - c).y == Approx(-9.3f));
+}
+
+TEST_CASE("describe_direktMultiplicationVec2", "[vec2]")
+{
+	Vec2 a;
+	Vec2 b{ 5.1f, -9.3f };
+	Vec2 c{ 6.61f, 0.1f };
+
+	REQUIRE((a * 0).x == Approx(0.0f));
+	REQUIRE((a * 0).y == Approx(0.0f));
+
+	REQUIRE((b * 0).x == Approx(0.0f));
+	REQUIRE((b * 0).y == Approx(0.0f));
+
+	REQUIRE((b * 1).x == Approx(5.1f));
+	REQUIRE((b * 1).y == Approx(-9.3f));
+
+	REQUIRE((b * 1).x == Approx(5.1f));
+	REQUIRE((b * 1).y == Approx(-9.3f));
+
+
+	REQUIRE((b * -1).x == Approx(-5.1f));
+	REQUIRE((b * -1).y == Approx(9.3f));
+
+
+	REQUIRE((b * 10).x == Approx(51.0f));
+	REQUIRE((b * 10).y == Approx(-93.0f));
+
+	REQUIRE((10 * c).x == Approx(66.1f));
+	REQUIRE((10 * c).y == Approx(1.0f));
+
+	REQUIRE((2.1f * c).x == Approx(13.881f));
+	REQUIRE((2.1f * c).y == Approx(0.21f));
+
+}
+
+TEST_CASE("describe_direktMultiplicationVec2", "[vec2]")
+{
+	Vec2 a;
+	Vec2 b{ 5.1f, -9.3f };
+
+	REQUIRE((a / 10.1).x == Approx(0.0f));
+	REQUIRE((a / 10.1).y == Approx(0.0f));
+
+	REQUIRE((b / 10).x == Approx(0.51f));
+	REQUIRE((b / 10).y == Approx(-0.93f));
+
+	REQUIRE((b / -10).x == Approx(-0.51f));
+	REQUIRE((b / -10).y == Approx(0.93f));
+}
 
 int main(int argc, char *argv[])
 {
