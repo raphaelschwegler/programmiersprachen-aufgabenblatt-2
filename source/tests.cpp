e643655db490f06a9e19beac8b8a81c39a2a7641
raphaelschwegler <raphael.schwegler@uni-weimar.de>
2020-05-20 12:52:02 +0200

    Added test cases Aufgabe 2.3 Vec2

--- source/tests.cpp
+++ source/tests.cpp
@@ -38,6 +38,76 @@ TEST_CASE("describe_additionVec2", "[vec2]")
 
 }
 
+TEST_CASE("describe_subtractionVec2", "[vec2]")
+{
+	Vec2 a;
+	Vec2 b{ 5.1f, -9.3f };
+	Vec2 c;
+	Vec2 d{ 3.2f, 2.1f };
+	a -= c;
+	REQUIRE(a.x == Approx(0.0f));
+	REQUIRE(a.y == Approx(0.0f));
+
+	a -= b;
+	REQUIRE(a.x == Approx(-5.1f));
+	REQUIRE(a.y == Approx(9.3f));
+
+	b += d;
+	REQUIRE(a.x == Approx(1.9f));
+	REQUIRE(a.y == Approx(-11.4f));
+
+	d += c;
+	REQUIRE(a.x == Approx(-3.2f));
+	REQUIRE(a.y == Approx(-2.1f));
+
+
+}
+
+TEST_CASE("describe_scalarMultVec2", "[vec2]")
+{
+	Vec2 a;
+	Vec2 b{ 5.1f, -9.3f };
+
+	REQUIRE((a*0.0f).x == Approx(0.0f));
+	REQUIRE((a*0.0f).y == Approx(0.0f));
+
+	REQUIRE((a * 20.0f).x == Approx(0.0f));
+	REQUIRE((a * -20.0f).y == Approx(0.0f));
+
+	REQUIRE((b * 2.0f).x == Approx(10.2f));
+	REQUIRE((b * 2.0f).y == Approx(-18.6f));
+
+	REQUIRE((b * -1.0f).x == Approx(-5.1f));
+	REQUIRE((b * -1.0f).y == Approx(9.3f));
+
+	REQUIRE((b * 0.5f).x == Approx(2.55f));
+	REQUIRE((b * 0.5f).y == Approx(4.65f));
+
+
+}
+
+TEST_CASE("describe_scalarDivisVec2", "[vec2]")
+{
+	Vec2 a;
+	Vec2 b{ 5.1f, -9.3f };
+
+	
+
+	REQUIRE((a / 20.0f).x == Approx(0.0f));
+	REQUIRE((a / -20.0f).y == Approx(0.0f));
+
+	REQUIRE((b / 0.5f).x == Approx(10.2f));
+	REQUIRE((b / 0.5f).y == Approx(-18.6f));
+
+	REQUIRE((b / -1.0f).x == Approx(-5.1f));
+	REQUIRE((b / -1.0f).y == Approx(9.3f));
+
+	REQUIRE((b / 2.0f).x == Approx(2.55f));
+	REQUIRE((b / 2.0f).y == Approx(4.65f));
+
+
+}
+
 
 
 int main(int argc, char *argv[])
